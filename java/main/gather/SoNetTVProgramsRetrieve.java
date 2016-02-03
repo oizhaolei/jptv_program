b59c3861bc8cc6e1090467d7327ec667074533ad
oizhaolei <oizhaolei@gmail.com>
2016-02-03 21:03:02 +0800

    sonet

---
+++ java/main/gather/SoNetTVProgramsRetrieve.java
@@ -0,0 +1,369 @@
+package gather;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.mysql.jdbc.StringUtils;
+
+public class SoNetTVProgramsRetrieve {
+	static String url = "http://tv.so-net.ne.jp/chart/%s.action?head=%s&span=1&sticky=false&descriptive=true";
+
+	static String tokyo_chijo_url = "http://tv.so-net.ne.jp/chart/23.action?head=%s&span=24&sticky=false&cellHeight=5&descriptive=true";
+	static String osaka_chijo_url = "http://tv.so-net.ne.jp/chart/40.action?head=%s&span=24&sticky=false&cellHeight=5&descriptive=true";
+	static String kobe_chijo_url = "http://tv.so-net.ne.jp/chart/42.action?head=%s&span=24&sticky=false&cellHeight=5&descriptive=true";
+	static String bs1 = "http://tv.so-net.ne.jp/chart/bs1.action?head=%s&span=24&sticky=false&cellHeight=5&descriptive=true";
+	static String bs2 = "http://tv.so-net.ne.jp/chart/bs2.action?head=%s&span=24&sticky=false&cellHeight=5&descriptive=true";
+	static String bs3 = "http://tv.so-net.ne.jp/chart/bs3.action?head=%s&span=24&sticky=false&cellHeight=5&descriptive=true";
+	static String bs4 = "http://tv.so-net.ne.jp/chart/bs4.action?head=%s&span=24&sticky=false&cellHeight=5&descriptive=true";
+
+	public final static DateFormat DB_DATETIME_FORMATTER = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm", Locale.JAPAN);
+	public final static DateFormat DB_DATETIME_FORMATTER2 = new SimpleDateFormat(
+			"yyyy-MM-dd HH", Locale.JAPAN);
+	public final static DateFormat DB_DATETIME_FORMATTER3 = new SimpleDateFormat(
+			"HH:mm", Locale.JAPAN);
+	public final static DateFormat DB_DATETIME_FORMATTER4 = new SimpleDateFormat(
+			"yyyyMMdd", Locale.JAPAN);
+	public final static DateFormat DB_DATETIME_FORMATTER5 = new SimpleDateFormat(
+			"yyyy-MM-dd", Locale.JAPAN);
+
+	static Connection conn;
+
+	static PreparedStatement existsCheckPS;
+	static PreparedStatement prevProgramPS;
+	static PreparedStatement insertPS;
+	static PreparedStatement deletePS;
+
+	public static Date date;
+
+	public static String[] channelNames = { "ＮＨＫ総合・東京", "ＮＨＫＥテレ１・東京", "日テレ",
+			"テレビ朝日", "ＴＢＳ", "テレビ東京", "フジテレビ", "ＴＯＫＹＯ　ＭＸ１", "放送大学１", "ＮＨＫ ＢＳ１",
+			"ＮＨＫ ＢＳプレミアム", "ＢＳ日テレ", "ＢＳ朝日", "ＢＳ-ＴＢＳ", "ＢＳジャパン", "ＢＳフジ",
+			"ＷＯＷＯＷプライム", "ＷＯＷＯＷライブ", "ＷＯＷＯＷシネマ", "スター・チャンネル1", "スター・チャンネル2",
+			"スター・チャンネル3", "ＢＳ１１", "BS12 トゥエルビ", "放送大学BS1", "グリーンチャンネル",
+			"BSアニマックス", "FOXスポーツエンタ", "BSスカパー!", "J SPORTS 1", "J SPORTS 2",
+			"J SPORTS 3", "J SPORTS 4", "BS釣りビジョン", "イマジカBS", "BS日本映画専門チャンネル",
+			"ディズニー・チャンネル", "ディーライフ", "ウェザーニュース", "ＮＨＫ総合・大阪", "ＮＨＫＥテレ１・大阪",
+			"ＭＢＳ毎日放送", "ＡＢＣテレビ", "テレビ大阪", "関西テレビ", "よみうりテレビ", "ＮＨＫ総合・神戸",
+			"サンテレビ" };
+
+	private static void delete(String date) throws ParseException, SQLException {
+		for(String channelName : channelNames) {
+			List<ChannelProgram> cps = ChannelProgram
+					.onSetChannelname(channelName);
+			//
+			Date dt = DB_DATETIME_FORMATTER4.parse(date);
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(dt);
+			calendar.add(Calendar.DATE, 1);
+			String date1 = DB_DATETIME_FORMATTER4.format(calendar.getTime());
+			date1 = date1.substring(0, 4) + "-" + date1.substring(4, 6) + "-"
+					+ date1.substring(6, 8) + " " + "00:00";
+			String date2 = date.substring(0, 4) + "-" + date.substring(4, 6) + "-"
+					+ date.substring(6, 8) + " " + "00:00";
+			for (ChannelProgram cp : cps) {
+				deletePS.setInt(1, cp.channelid);
+				deletePS.setString(2, date1);
+				deletePS.setString(3, date2);
+				deletePS.executeUpdate();
+			}
+		}
+	}
+
+
+	/**
+	 * 获得指定日期的间隔日期
+	 *
+	 * @param specifiedDay
+	 * @return
+	 */
+	public static Date getDateAfterSpecifiedDay(Date specifiedDay, int days) {
+		Calendar c = Calendar.getInstance();
+
+		c.setTime(specifiedDay);
+		int day = c.get(Calendar.DATE);
+		c.set(Calendar.DATE, day + days);
+
+		return c.getTime();
+	}
+
+	private static int getHeightByStyle(String css) {
+		int height = 0;
+		String px = parseCssStyle(css, "height");
+		if (!StringUtils.isNullOrEmpty(px) && px.indexOf("px") > -1) {
+			px = px.substring(0, px.length() - 2);
+			height = new Integer(px).intValue();
+		}
+		return height;
+	}
+
+	private static int getProgramHour(int top) {
+		return ( top -20 ) / 300;
+	}
+
+	private static int getValueByCss(String css, String prop) {
+		int height = 0;
+		String px = parseCssStyle(css, prop);
+		if (!StringUtils.isNullOrEmpty(px) && px.indexOf("px") > -1) {
+			px = px.substring(0, px.length() - 2);
+			height = new Integer(px).intValue();
+		}
+		return height;
+	}
+
+	public static void help() {
+		DBclass.print("java -cp tvlist_gather.jar gather.SoNetTVProgramsRetrieve");
+		DBclass.print("java -cp tvlist_gather.jar gather.SoNetTVProgramsRetrieve");
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		try {
+			DBclass.print("now: %s",
+					DateFormat.getDateTimeInstance().format(new Date()));
+			date = new Date();
+
+			conn = DBclass.getConn();
+			existsCheckPS = conn
+					.prepareStatement("select count(0) from tbl_channel_program where channelid=? and program_time=?");
+			prevProgramPS = conn
+					.prepareStatement("select max(program_time) from tbl_channel_program where channelid=? and program_time<?");
+			insertPS = conn
+					.prepareStatement("insert into tbl_channel_program (channelid, title, contents, program_time, create_id, create_date, update_id, update_date) values (?, ?, ?, ?, 'cron', now(), 'cron', now())");
+			deletePS = conn.prepareStatement("DELETE from tbl_channel_program where channelid=? and program_time < ? and program_time >= ?");
+			// retrieve
+
+			// args = new String[] { "2" };
+			int days = 0;
+
+			if (args.length > 0) {
+				days = Integer.valueOf(args[0]);
+			} else {
+				days = 0;
+			}
+
+			for (int i = 0; i <= days; i++) {
+				Date program_date = getDateAfterSpecifiedDay(new Date(), i);
+				String timeStr = DB_DATETIME_FORMATTER4.format(program_date)
+						+ "0000";
+				delete(DB_DATETIME_FORMATTER4.format(program_date));
+				retrieveProgramByUrl(String.format(tokyo_chijo_url, timeStr),
+						program_date);
+				retrieveProgramByUrl(String.format(osaka_chijo_url, timeStr),
+						program_date);
+				retrieveProgramByUrl(String.format(kobe_chijo_url, timeStr),
+						program_date);
+				retrieveProgramByUrl(String.format(bs1, timeStr), program_date);
+				retrieveProgramByUrl(String.format(bs2, timeStr), program_date);
+				retrieveProgramByUrl(String.format(bs3, timeStr), program_date);
+				retrieveProgramByUrl(String.format(bs4, timeStr), program_date);
+			}
+
+			deletePS.close();
+			existsCheckPS.close();
+			insertPS.executeBatch();
+
+			// 删除超过8天以上的数据
+			conn.createStatement()
+					.execute(
+							"delete from tbl_channel_program where  DATEDIFF(now(), program_time) >8");
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		} finally {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		}
+		DBclass.print("-- over --");
+	}
+
+	private static String parseCssStyle(String str, String key) {
+		String value = null;
+		str = str.replaceAll("\\r\\n", "").replace(" ", "");
+		String[] arr = str.split(";");
+		for (String style : arr) {
+			String[] prop = style.split(":");
+			if (prop[0].equalsIgnoreCase(key)) {
+				value = prop[1];
+			}
+		}
+		return value;
+	}
+
+	private static void parseDoc(Document doc, Date program_date)
+			throws SQLException, ParseException {
+		String channelName = null;
+		String title = null;
+		String contents = null;
+		String program_time = null;
+		int program_hour = 0;
+		boolean timeFlg = true;
+		int sum = 0;
+		Elements charts = doc.getElementById("chartColumn").children();
+		for (int i = 0; i < charts.size(); i++) {
+			Element div = charts.get(i);
+			String attr = div.attr("class");
+			if (("cell-station cell-top").equals(attr)) {
+				// 电视台
+				sum = 0;
+				channelName = DBclass.xmlFilte(div.attr("title"));
+				title = null;
+				contents = null;
+				program_time = null;
+				program_hour = 0;
+
+			} else if (attr != null && attr.indexOf("cell-schedule") > -1
+					&& attr.indexOf("system-cell-schedule-head") > -1) {
+				// 节目
+				String style = div.attr("style");
+				int height = getHeightByStyle(style);
+				int top = getValueByCss(style, "top");
+
+				Elements detail = div.select("table").select("tr").select("td");
+				for (Element program : detail) {
+					if (("td-minute").equals(program.attr("class"))) {
+						// 节目时间
+						String min = DBclass.xmlFilte(program.text()).trim();
+						if (min.length() == 2) {
+							// if (!("00").equals(min)) {
+							// int h = sum / 297;
+							// if (h > program_hour) {
+							// program_hour = h;
+							// }
+							//
+							// } else {
+							// if (sum == 0) {
+							// program_hour = 0;
+							// } else if (sum > 0) {
+							// int h = sum / 297;
+							// int rest = sum % 297;
+							// if (rest > 0) {
+							// program_hour = h + 1;
+							// } else {
+							// program_hour = h;
+							// }
+							// }
+							//
+							// }
+
+							program_hour = getProgramHour(top);
+
+							String hour = program_hour > 9 ? ("" + program_hour)
+									: ("0" + program_hour);
+							program_time = DB_DATETIME_FORMATTER5
+									.format(program_date)
+									+ " "
+									+ hour
+									+ ":"
+									+ min;
+
+
+						} else if (min.length() > 2 && min.indexOf(":") > -1) {
+							Elements els = program.select("span");
+							for (Element el : els) {
+								if (("td-minute").equals(el.attr("class"))) {
+									min = DBclass.xmlFilte(el.text()).trim();
+									program_time = DB_DATETIME_FORMATTER5
+											.format(getDateAfterSpecifiedDay(
+													program_date, -1))
+											+ " " + min;
+								} else if (("schedule-titleC").equals(el
+										.attr("class"))) {
+									// 节目标题
+									title = DBclass.xmlFilte(el.text());
+								} else if (("schedule-summaryC").equals(el
+										.attr("class"))) {
+									// 节目简介
+									contents = DBclass.xmlFilte(el.text());
+								}
+							}
+
+							sum = sum + height;
+						}
+
+					} else if (("td-schedule").equals(program.attr("class"))) {
+						// 节目内容
+						Elements els = program.select("span");
+						for (Element el : els) {
+							if (("schedule-title").equals(el.attr("class"))) {
+								// 节目标题
+								title = DBclass.xmlFilte(el.text());
+							} else if (("schedule-summary").equals(el
+									.attr("class"))) {
+								// 节目简介
+								contents = DBclass.xmlFilte(el.text());
+							}
+						}
+
+						sum = sum + height;
+					}
+				}
+
+				List<ChannelProgram> cps = ChannelProgram
+						.onSetChannelname(channelName);
+				for (ChannelProgram cp : cps) {
+					cp.program_time = program_time;
+					cp.title = title;
+					cp.contents = contents;
+					if (cp.channelid != -1 && timeFlg) {
+						DBclass.addToDb(cp, prevProgramPS, insertPS,
+								existsCheckPS);
+					} else {
+						DBclass.print("ignore:%s, %s, %s, %s", cp.channelid,
+								cp.title, cp.program_time, timeFlg);
+					}
+				}
+
+			}
+
+		}
+
+	}
+
+	private static void retrieveProgramByUrl(String url, Date program_date)
+			throws IOException {
+
+		Document doc = null;
+		for (int i = 0; doc == null && i < 5; i++) {
+			DBclass.print("%d. retrieving %s", i + 1, url);
+			try {
+				doc = Jsoup
+						.connect(url)
+						.userAgent(
+								"Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0")
+						.timeout(50000).get();
+			} catch (Exception e1) {
+				e1.printStackTrace(System.out);
+			}
+		}
+
+		try {
+			parseDoc(doc, program_date);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ java/main/gather/YahooProgramsRetrieve.java
@@ -0,0 +1,382 @@
+package gather;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.mysql.jdbc.StringUtils;
+
+/**
+ *
+ * @author Administrator
+ *
+ */
+public class YahooProgramsRetrieve {
+	static String url = "http://tv.yahoo.co.jp/listings/%s/?st=4&s=1&va=24&vc=0&vd=0&ve=0&vb=0&a=23&d=%s";
+
+	public final static DateFormat DB_DATETIME_FORMATTER = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm", Locale.JAPAN);
+	public final static DateFormat DB_DATETIME_FORMATTER2 = new SimpleDateFormat(
+			"yyyy-MM-dd HH", Locale.JAPAN);
+	public final static DateFormat DB_DATETIME_FORMATTER3 = new SimpleDateFormat(
+			"HH:mm", Locale.JAPAN);
+	public final static DateFormat DB_DATETIME_FORMATTER4 = new SimpleDateFormat(
+			"yyyyMMdd", Locale.JAPAN);
+
+	static Connection conn;
+	static PreparedStatement existsCheckPS;
+	static PreparedStatement prevProgramPS;
+	static PreparedStatement insertPS;
+	static PreparedStatement deletePS;
+	public static String today;
+
+	public static String[] channelsetArray = { "23 ", "bs1", "bs2", "bs3",
+			"bs4", "bs5", "nottv" };
+	public static List<String[][]> channelList = new ArrayList<String[][]>();
+	public static String[][] channelArray_tokyo = { { "1ch", "NHK総合・東京" },
+			{ "2ch", "NHKEテレ東京" }, { "4ch", "日テレ" }, { "5ch", "テレビ朝日" },
+			{ "6ch", "TBS" }, { "7ch", "テレビ東京" }, { "8ch", "フジテレビ" },
+			{ "9ch", "TOKYO　MX" }, { "12ch", "放送大学" } };
+	public static String[][] channelArray_bs1 = { { "101ch", "NHKBS1" },
+			{ "103ch", "NHKBSプレミアム" }, { "141ch", "BS日テレ" },
+			{ "151ch", "BS朝日1" }, { "161ch", "BS-TBS" }, { "171ch", "BSジャパン" },
+			{ "181ch", "BSフジ・181" } };
+	public static String[][] channelArray_bs2 = { { "191ch", "WOWOWプライム" },
+			{ "192ch", "WOWOWライブ" }, { "193ch", "WOWOWシネマ" },
+			{ "200ch", "スターチャンネル1" }, { "201ch", "スターチャンネル2" },
+			{ "202ch", "スターチャンネル3" }, { "211ch", "BS11" },
+			{ "222ch", "BS12トゥエルビ" } };
+	public static String[][] channelArray_bs3 = { { "231ch", "放送大学BS1" },
+			{ "232ch", "放送大学BS2" }, { "233ch", "放送大学BS3" },
+			{ "234ch", "グリーンチャンネル" }, { "236ch", "BSアニマックス" },
+			{ "238ch", "FOXスポーツエンタ" }, { "241ch", "BSスカパー!" } };
+	public static String[][] channelArray_bs4 = { { "242ch", "J SPORTS 1" },
+			{ "243ch", "J SPORTS 2" }, { "244ch", "J SPORTS 3" },
+			{ "245ch", "J SPORTS 4" }, { "251ch", "BS釣りビジョン" },
+			{ "252ch", "イマジカBS・映画" }, { "255ch", "BS日本映画専門ch" } };
+	public static String[][] channelArray_bs5 = { { "256ch", "ディズニーチャンネル" },
+			{ "258ch", "ディーライフ" }, { "531ch", "放送大学ラジオ" } };
+	public static String[][] channelArray_nottv = { { "nottv1", "NOTTV1" },
+			{ "nottv2", "NOTTV2byホウドウキョク24" },
+			{ "nottv3", "フジテレビONEスポーツ・バラエティ" },
+			{ "nottv4", "フジテレビTWOドラマ・アニメ" }, { "nottv5", "時代劇専門チャンネル" },
+			{ "nottv6", "AXN　海外ドラマ" }, { "nottv7", "アニマックス" },
+			{ "nottv8", "スカサカ！24時間サッカー専門チャンネル" } };
+
+	private static void delete(String date) throws Exception {
+		List<ChannelProgram> cps = ChannelProgram
+				.onSetChannelname("ｺﾞﾙﾌﾈｯﾄﾜｰｸ");
+		Date dt = DB_DATETIME_FORMATTER4.parse(date);
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dt);
+		calendar.add(Calendar.DAY_OF_MONTH, 1);
+		String date1 = DB_DATETIME_FORMATTER4.format(calendar.getTime());
+		date1 = date1.substring(0, 4) + "-" + date1.substring(4, 6) + "-"
+				+ date1.substring(6, 8) + " " + "04:00";
+		String date2 = date.substring(0, 4) + "-" + date.substring(4, 6) + "-"
+				+ date.substring(6, 8) + " " + "04:00";
+		for (ChannelProgram cp : cps) {
+			deletePS.setInt(1, cp.channelid);
+			deletePS.setString(2, date1);
+			deletePS.setString(3, date2);
+			deletePS.executeUpdate();
+		}
+
+		deletePS.close();
+	}
+
+	public static void help() {
+		DBclass.print("java -cp tvlist_gather.jar gather.TVGolfRetrieve");
+		DBclass.print("java -cp tvlist_gather.jar gather.TVGolfRetrieve");
+	}
+
+	public static void initChannelList() {
+		channelList.add(channelArray_tokyo);
+		channelList.add(channelArray_bs1);
+		channelList.add(channelArray_bs2);
+		channelList.add(channelArray_bs3);
+		channelList.add(channelArray_bs4);
+		channelList.add(channelArray_bs5);
+		channelList.add(channelArray_nottv);
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		try {
+			initChannelList();
+
+			DBclass.print("now: %s",
+					DateFormat.getDateTimeInstance().format(new Date()));
+			today = DB_DATETIME_FORMATTER4.format(new Date());
+
+			args = new String[] { "20160113", "bs1" };
+			String dateStr;
+			if (args.length > 0) {
+				dateStr = args[0];
+			} else {
+				dateStr = DB_DATETIME_FORMATTER4.format(new Date());
+			}
+
+			String channelStr;
+			if (args.length > 1) {
+				channelStr = args[1];
+			} else {
+				channelStr = "bs1";
+			}
+
+			url = String.format(url, channelStr, dateStr);
+
+			// conn = DBclass.getConn();
+			// existsCheckPS = conn
+			// .prepareStatement("select count(0) from tbl_channel_program where channelid=? and program_time=?");
+			// prevProgramPS = conn
+			// .prepareStatement("select max(program_time) from tbl_channel_program where channelid=? and program_time<?");
+			// insertPS = conn
+			// .prepareStatement("insert into tbl_channel_program (channelid, title, contents, program_time, create_id, create_date, update_id, update_date) values (?, ?, ?, ?, 'cron', now(), 'cron', now())");
+			// // retrieve
+			// deletePS = conn
+			// .prepareStatement("DELETE from tbl_channel_program where channelid=? and program_time < ? and program_time >= ?");
+			// delete(today);
+
+			url = "http://tv.yahoo.co.jp/ajax/listings/?.bcrumb=dD1YZ0dsV0Imc2s9ZkRORjREWGR5LjFpRk5uMk10Qzd1c0kwV3ZJLQ==&type=normal&a=23&t=BS1&d=20160113&st=4&s=1&va=24&vb=0&vc=0&vd=0&ve=0";
+			retrieveChannel(url, channelStr);
+
+			// existsCheckPS.close();
+			// insertPS.executeBatch();
+
+			// // 删除超过8天以上的数据
+			// conn.createStatement()
+			// .execute(
+			// "delete from tbl_channel_program where  DATEDIFF(now(), program_time) >8");
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		} finally {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		}
+		DBclass.print("-- over --");
+	}
+
+	private static int parseChannel(Elements channelElements, int channelset,
+			String channelname)
+			throws ParseException {
+		int column = 0;
+
+		Elements channelNotes = channelElements.select("td");
+		for (int j = 1; j < channelNotes.size(); j++) {
+			Element el = channelNotes.get(j);
+			String temp = DBclass.xmlFilte(el.text());
+			if (temp.indexOf(channelname) > -1) {
+				column = j;
+				break;
+			}
+		}
+
+		return column;
+	}
+
+	private static int parseChannel(String channelname) throws ParseException {
+		int column = 0;
+
+		return column;
+	}
+
+	/**
+	 * 解析数据
+	 *
+	 * @param rows
+	 *            源数据集
+	 * @param column
+	 *            被解释数据所在列
+	 * @return 节目数据
+	 */
+	private static String[][] parseRows(Elements rows, int column) {
+
+		List<List<Integer>> rowspan = new ArrayList<List<Integer>>();
+		List<List<String[]>> programsList = new ArrayList<List<String[]>>();
+
+		String[][] temp_programs = new String[rows.size()][2];
+		String[][] programs = temp_programs;
+		int[] rowspan_rest = new int[8];
+
+		for (int i = 0; i < rows.size(); i++) {
+			Element row = rows.get(i);
+
+			try {
+				Elements cells = row.children();
+
+				List<Integer> rowspanlist = new ArrayList<Integer>();
+				if (null != cells && cells.size() > 0) {
+					List<String[]> data = new ArrayList<String[]>();
+					for (int m = 0; m < 8; m++) {
+
+						if (rowspan_rest[m] > 1) {
+							rowspanlist.add(Integer.valueOf(0));
+							String[] program = { "", "" };
+							data.add(program);
+
+							rowspan_rest[m]--;
+
+						} else {
+							Element cell = cells.get(0);
+							Integer span = Integer
+									.valueOf(cell.attr("rowspan"));
+							rowspanlist.add(span);
+							String[] program = new String[2];
+							program[0] = DBclass.xmlFilte(cell.select("dt")
+									.text());
+							program[1] = DBclass.xmlFilte(cell.select("dd")
+									.text());
+							data.add(program);
+
+							cells.remove(0);
+
+							rowspan_rest[m] = span.intValue();
+						}
+
+					}
+
+					programsList.add(data);
+
+				} else {
+					List<String[]> data = new ArrayList<String[]>();
+					for (int r = 0; r < 8; r++) {
+						rowspanlist.add(Integer.valueOf(0));
+						String[] program = { "", "" };
+						data.add(program);
+
+						rowspan_rest[r]--;
+					}
+
+					programsList.add(data);
+				}
+
+				rowspan.add(rowspanlist);
+
+			} catch (Exception e) {
+				e.printStackTrace(System.out);
+			}
+
+		}
+
+		int l = 0;
+		for (int n = 0; n < programsList.size(); n++, l++) {
+			List<String[]> data = programsList.get(n);
+			String[] program = data.get(column);
+			if (!StringUtils.isNullOrEmpty(program[0])
+					&& !StringUtils.isNullOrEmpty(program[1])) {
+				temp_programs[l][0] = program[0];
+				temp_programs[l][1] = program[1];
+			} else {
+				l--;
+			}
+		}
+
+		programs = new String[l][2];
+		for (int m = 0; m < l; m++) {
+			programs[m][0] = temp_programs[m][0];
+			programs[m][1] = temp_programs[m][1];
+		}
+
+		return programs;
+	}
+
+	private static void retrieveChannel(String url, String channelset)
+			throws Exception {
+
+		Document doc = null;
+		for (int i = 0; doc == null && i < 5; i++) {
+			DBclass.print("%d. retrieving %s", i + 1, url);
+			try {
+				doc = Jsoup
+						.connect(url)
+						.userAgent(
+								"Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0")
+						.timeout(50000).get();
+			} catch (Exception e1) {
+				e1.printStackTrace(System.out);
+			}
+		}
+
+		// Element content = doc.getElementById("Tables");
+		// Element tables = doc.child(1).child(2).child(7);
+		// http://tv.yahoo.co.jp/ajax/listings/?.bcrumb=dD1YZ0dsV0Imc2s9ZkRORjREWGR5LjFpRk5uMk10Qzd1c0kwV3ZJLQ==&type=normal&a=23&t=BS1&d=20160113&st=4&s=1&va=24&vb=0&vc=0&vd=0&ve=0
+		// Elements scripts = doc.getElementsByTag("script");
+
+		Elements channels = doc.getElementById("ListingsHeader")
+				.select("tbody").select("tr");
+
+		// TODO ... 循环查找频道
+
+		int channelcol = parseChannel(channels, 0, "101ch");
+
+		Elements rows = doc.select("tbody").select("tr");
+		String[][] programs = parseRows(rows, channelcol);
+
+
+
+		Date dt = DB_DATETIME_FORMATTER4.parse(today);
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dt);
+		calendar.add(Calendar.DAY_OF_MONTH, 1);
+		String nextDay = DB_DATETIME_FORMATTER4.format(calendar.getTime());
+
+		for (int i = 0; i < programs.length; i++) {
+			String[] program = programs[i];
+			if (program[0] != null && program[0].length() > 0) {
+				String time = program[0].substring(0, 5);
+				String program_time;
+				Integer hour = Integer.valueOf(time.substring(0, 2));
+				if (hour < 24) {
+					program_time = today.substring(0, 4) + "-"
+							+ today.substring(4, 6) + "-"
+							+ today.substring(6, 8) + " " + time;
+				} else {
+					time = (hour - 24) > 10 ? String.valueOf(hour - 24)
+							+ time.substring(2) : "0"
+							+ String.valueOf(hour - 24) + time.substring(2);
+					program_time = nextDay.substring(0, 4) + "-"
+							+ nextDay.substring(4, 6) + "-"
+							+ nextDay.substring(6, 8) + " " + time;
+				}
+				String title = program[1];
+				if (title.length() > 16)
+					title = title.substring(0, 16);
+				String contents = program[1];
+				if (contents.length() > 66)
+					contents = contents.substring(0, 66);
+
+				List<ChannelProgram> cps = ChannelProgram
+						.onSetChannelname("ｺﾞﾙﾌﾈｯﾄﾜｰｸ");
+				for (ChannelProgram cp : cps) {
+					cp.program_time = program_time;
+					cp.title = title;
+					cp.contents = contents;
+					DBclass.print(cp.toString());
+
+					if (cp.channelid != -1)
+						DBclass.addToDb(cp, prevProgramPS, insertPS,
+								existsCheckPS);
+				}
+			}
+		}
+	}
+
+}
